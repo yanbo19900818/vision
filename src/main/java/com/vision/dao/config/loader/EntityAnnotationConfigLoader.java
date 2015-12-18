5b593c7501a12495da0153f99e8816006fc5b7b5
yanbo19900818 <yanbo19900818@gmail.com>
2015-12-18 15:17:00 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.gitignore

--- src/main/java/com/vision/dao/config/loader/EntityAnnotationConfigLoader.java
+++ src/main/java/com/vision/dao/config/loader/EntityAnnotationConfigLoader.java
@@ -9,10 +9,8 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by yanbo on 15/9/13.
@@ -20,48 +18,46 @@ import java.util.Map;
 public class EntityAnnotationConfigLoader {
 
     public EntityConfig loadEntity(String ClassName) {
-        Class<?> c = null;
         try {
-            c = Class.forName(ClassName);
+            Class<?> c = Class.forName(ClassName);
+            EntityConfig entityConfig = new EntityConfig();
+            Entity entity = c.getAnnotation(Entity.class);
+            //checkAnnotation
+            if (StringUtils.isBlank(entity.databaseName()))
+                throw new DaoException("load entity " + ClassName + "faile, db Name is Null");
+            if (StringUtils.isBlank(entity.tableName()))
+                throw new DaoException("load entity " + ClassName + "faile, table Name is Null");
+            if (entity.isSharding()) {
+                if (StringUtils.isBlank(entity.shardingScheme())) {
+                    throw new DaoException("load entity " + ClassName + "faile, shardingScheme is Null");
+                }
+            }
+            entityConfig.setDatabaseName(entity.databaseName());
+            entityConfig.setTableName(entity.tableName());
+            if (entity.isSharding()) {
+                entityConfig.setSharding(true);
+                entityConfig.setShardingScheme(entity.shardingScheme());
+            }
+            List<Field> fields = Arrays.asList(c.getDeclaredFields());
+            if (fields == null)
+                throw new DaoException("load entity " + ClassName + "faile, no field");
+            Map<String, EntityFieldConfig> entityFieldConfigMap = new HashMap<>();
+            fields.stream().collect(Collectors.toMap(Field::getName, e -> {
+                EntityField entityField = e.getAnnotation(EntityField.class);
+                EntityFieldConfig entityFieldConfig = new EntityFieldConfig();
+                if (entityField == null) {
+                    entityFieldConfig.setEntityColumnName(e.getName());
+                    entityFieldConfig.setDatabaseColumnType(e.getType().getName());
+                } else {
+                    entityFieldConfig.setDatabaseColumnName(entityField.databaseColumnName());
+                    entityFieldConfig.setDatabaseColumnType(entityField.databaseColumnType());
+                }
+                return entityConfig;
+            }));
+            entityConfig.setEntityFieldConfigMap(entityFieldConfigMap);
+            return entityConfig;
         } catch (ClassNotFoundException e) {
             throw new DaoException("load entity " + ClassName + "faile, class not found", e);
         }
-        EntityConfig entityConfig = new EntityConfig();
-        Entity entity = c.getAnnotation(Entity.class);
-        //checkAnnotation
-        if (StringUtils.isBlank(entity.databaseName()))
-            throw new DaoException("load entity " + ClassName + "faile, db Name is Null");
-        if (StringUtils.isBlank(entity.tableName()))
-            throw new DaoException("load entity " + ClassName + "faile, table Name is Null");
-        if (entity.isSharding()) {
-            if (StringUtils.isBlank(entity.shardingScheme())) {
-                throw new DaoException("load entity " + ClassName + "faile, shardingScheme is Null");
-            }
-        }
-        entityConfig.setDatabaseName(entity.databaseName());
-        entityConfig.setTableName(entity.tableName());
-        if (entity.isSharding()) {
-            entityConfig.setSharding(true);
-            entityConfig.setShardingScheme(entity.shardingScheme());
-        }
-        Field[] fields = c.getDeclaredFields();
-        if (fields == null || fields.length <= 0)
-            throw new DaoException("load entity " + ClassName + "faile, no field");
-        Map<String, EntityFieldConfig> entityFieldConfigMap = new HashMap<>();
-        for (Field field : fields) {
-            EntityField entityField = field.getAnnotation(EntityField.class);
-            EntityFieldConfig entityFieldConfig = new EntityFieldConfig();
-            if (entityField == null) {
-                entityFieldConfig.setEntityColumnName(field.getName());
-                entityFieldConfig.setDatabaseColumnType(field.getType().getName());
-            } else {
-                entityFieldConfig.setDatabaseColumnName(entityField.databaseColumnName());
-                entityFieldConfig.setDatabaseColumnType(entityField.databaseColumnType());
-            }
-            entityFieldConfigMap.put(field.getName(), entityFieldConfig);
-        }
-        entityConfig.setEntityFieldConfigMap(entityFieldConfigMap);
-        return entityConfig;
     }
-
 }
